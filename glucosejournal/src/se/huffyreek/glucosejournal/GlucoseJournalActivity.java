debca0b2e9e2bf355ec494396fccf035dccd06a7
Jimmie Elvenmark <flugsio@gmail.com>
2012-06-25 23:28:42 +0200

    First commit, empty project

---
+++ glucosejournal/src/se/huffyreek/glucosejournal/GlucoseJournalActivity.java
@@ -0,0 +1,13 @@
+package se.huffyreek.glucosejournal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlucoseJournalActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

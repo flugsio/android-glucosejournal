e19b7bb26cd8a41dd29c1567d8f36690a3f9e7d5
Jimmie Elvenmark <flugsio@gmail.com>
2012-10-21 16:49:08 +0200

    Helper function charAtIs, better layout for the list.

--- glucosejournal/src/se/huffyreek/glucosejournal/GlucoseJournalActivity.java
+++ glucosejournal/src/se/huffyreek/glucosejournal/GlucoseJournalActivity.java
@@ -7,12 +7,18 @@ import java.util.List;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.FocusFinder;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -48,7 +54,7 @@ public class GlucoseJournalActivity extends Activity {
     	
     	editGlucose.addTextChangedListener(new TextWatcher() {
 			public void afterTextChanged(Editable s) {
-				if(s.length() >= 4) {
+				if(s.length() >= 4 || charAtIs(s, -2, '.')) {
 					editCarbohydrates.requestFocus();
 				}
 			}
@@ -68,7 +74,7 @@ public class GlucoseJournalActivity extends Activity {
     	
     	editDose.addTextChangedListener(new TextWatcher() {
 			public void afterTextChanged(Editable s) {
-				if(s.length() >= 4 || (s.length() >= 2 && (s.charAt(s.length()-2) == '.'))) {
+				if(s.length() >= 4 || charAtIs(s, -2, '.')) {
 					buttonSave.callOnClick();
 				}
 			}
@@ -96,9 +102,61 @@ public class GlucoseJournalActivity extends Activity {
     	list_entries.removeAllViews();
     	
     	for (JournalEntry entry : journalEntries) {
-    		TextView tv = new TextView(this);
-    		tv.setText(entry.toString());
-    		list_entries.addView(tv);
+    		addRow(list_entries, entry.at.format("%H:%M"), entry.glucose, entry.carbohydrates, entry.dose);
     	}
+    }
+    
+    private void addRow(ViewGroup target, String str1, String str2, String str3, String str4) {
+    	DisplayMetrics metrics = new DisplayMetrics();
+    	getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    	
+		LinearLayout ll = new LinearLayout(this);
+		ll.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		ll.setOrientation(LinearLayout.HORIZONTAL);
+		
+		TextView tv1 = new TextView(this);
+		tv1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv1.setWidth((int)(metrics.widthPixels * 0.25));
+		tv1.setText(str1);
+		
+		TextView tv2 = new TextView(this);
+		tv2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv2.setWidth((int)(metrics.widthPixels * 0.30));
+		tv2.setText(str2);
+		
+		TextView tv3 = new TextView(this);
+		tv3.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv3.setWidth((int)(metrics.widthPixels * 0.25));
+		tv3.setText(str3);
+		
+		TextView tv4 = new TextView(this);
+		tv4.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv4.setWidth((int)(metrics.widthPixels * 0.20));
+		tv4.setText(str4);
+		
+		ll.addView(tv1);
+		ll.addView(tv2);
+		ll.addView(tv3);
+		ll.addView(tv4);
+		
+		target.addView(ll);
+    }
+
+
+    // Missing framework function, to easily check if char is in string at a position.
+    // Position can of course be negative, -1 equals last char and -2 is next to last.
+    // No exceptions, just true or false. Index out of boundaries will return false.
+    private boolean charAtIs(String s, int position, char c) {
+    	if(position < 0) {
+    		position = s.length() - (position * -1);
+    	}
+    	if(position < 0 || position >= s.length()) {
+    		return false;
+    	} else {
+    		return s.charAt(position) == c;
+    	}
+    }
+    private boolean charAtIs(Editable s, int position, char c) {
+    	return charAtIs(s.toString(), position, c);
     }
 }

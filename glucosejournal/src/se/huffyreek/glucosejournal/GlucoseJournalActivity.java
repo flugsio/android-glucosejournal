7ed5410000b18a5b984e6e479d681a3fb6dc2ee3
Jimmie Elvenmark <flugsio@gmail.com>
2013-07-27 06:10:13 +0200

    Edit functionality, context menu + click on latest

--- glucosejournal/src/se/huffyreek/glucosejournal/GlucoseJournalActivity.java
+++ glucosejournal/src/se/huffyreek/glucosejournal/GlucoseJournalActivity.java
@@ -18,11 +18,18 @@ import android.text.format.DateFormat;
 import android.text.format.Time;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.FocusFinder;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -32,6 +39,7 @@ public class GlucoseJournalActivity extends Activity {
 	private static final String TAG = "GlucoseJournalActivity";
 	
 	private List<JournalEntry> journalEntries;
+	private EditText editId;
 	private EditText editAt;
 	private EditText editGlucose;
 	private EditText editCarbohydrates;
@@ -44,6 +52,7 @@ public class GlucoseJournalActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    	createHeader();
         
         dbHandler = new GlucoseJournalDatabaseHelper(this);
 
@@ -54,11 +63,23 @@ public class GlucoseJournalActivity extends Activity {
     }
     
     private void initializeControls() {
+    	editId = (EditText) findViewById(R.id.edit_id);
     	editAt = (EditText) findViewById(R.id.edit_at);
     	editGlucose = (EditText) findViewById(R.id.edit_glucose);
     	editCarbohydrates = (EditText) findViewById(R.id.edit_carbohydrates);
     	editDose = (EditText) findViewById(R.id.edit_dose);
     	buttonSave = (Button) findViewById(R.id.button_save);
+    	
+    	editAt.addTextChangedListener(new TextWatcher() {
+			public void afterTextChanged(Editable s) {
+				if(s.length() >= 4) {
+					editGlucose.requestFocus();
+					editGlucose.selectAll();
+				}
+			}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			public void onTextChanged(CharSequence s, int start, int before, int count) {}
+    	});
     	
     	editGlucose.addTextChangedListener(new TextWatcher() {
 			public void afterTextChanged(Editable s) {
@@ -92,24 +113,46 @@ public class GlucoseJournalActivity extends Activity {
     }
     
     public void saveEntry(View view) {
-    	JournalEntry entry = new JournalEntry(editAt.getText().toString(), editGlucose.getText().toString(), editCarbohydrates.getText().toString(), editDose.getText().toString());
+    	if (editId.getText().toString().isEmpty()) {
+	    	JournalEntry entry = new JournalEntry(editAt.getText().toString(), editGlucose.getText().toString(), editCarbohydrates.getText().toString(), editDose.getText().toString());
 
-    	dbHandler.addJournalEntry(entry);
-    	
-    	// Return focus to glucose field unless time was filled; only for new records.
-    	if (editAt.getText().toString().isEmpty())
+	    	dbHandler.addJournalEntry(entry);
+	    	
+	    	// Return focus to glucose field unless time was filled; only for new records.
+	    	if (editAt.getText().toString().isEmpty())
+	    		editGlucose.requestFocus();
+	    	else
+	    		editAt.requestFocus();
+    	} else {
+    		
+    		JournalEntry entry = dbHandler.findJournalEntry(Integer.parseInt(editId.getText().toString()));
+    		entry.update_at_time(editAt.getText().toString());
+    		entry.glucose = editGlucose.getText().toString();
+    		entry.carbohydrates = editCarbohydrates.getText().toString();
+    		entry.dose = editDose.getText().toString();
+    		dbHandler.updateJournalEntry(entry);
+    		
     		editGlucose.requestFocus();
-    	else
-    		editAt.requestFocus();
+    	}
     	
+    	editId.setText("");
     	editAt.setText("");
     	editGlucose.setText("");
     	editCarbohydrates.setText("");
     	editDose.setText("");
     	
     	refreshEntries();
-    	
-    	editGlucose.requestFocus();
+    }
+    
+    private void editEntry(int id) {
+    	JournalEntry entry = dbHandler.findJournalEntry(id);
+    	editId.setText(Integer.toString(entry.id));
+    	editAt.setText(entry.at.format("%H%M"));
+    	editGlucose.setText(entry.glucose);
+    	editCarbohydrates.setText(entry.carbohydrates);
+    	editAt.requestFocus(); // Start editing "at"; need to come before updating "dose" to prevent auto-save and after updating "at" to keep the focus
+    	editAt.selectAll();
+    	editDose.setText(entry.dose);
     }
     
     private void refreshEntries() {
@@ -123,33 +166,89 @@ public class GlucoseJournalActivity extends Activity {
     	}
     }
     
-    private void addRow(ViewGroup target, String str1, String str2, String str3, String str4) {
+    private void createHeader() {
     	DisplayMetrics metrics = new DisplayMetrics();
     	getWindowManager().getDefaultDisplay().getMetrics(metrics);
     	
-		LinearLayout ll = new LinearLayout(this);
+    	LinearLayout ll = new LinearLayout(this);
 		ll.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		ll.setOrientation(LinearLayout.HORIZONTAL);
+		ll.setOrientation(LinearLayout.HORIZONTAL);	
 		
 		TextView tv1 = new TextView(this);
 		tv1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-		tv1.setWidth((int)(metrics.widthPixels * 0.25));
+		tv1.setWidth((int)(metrics.widthPixels * 0.15));
+		tv1.setText("Time");
+
+		TextView tv2 = new TextView(this);
+		tv2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv2.setWidth((int)(metrics.widthPixels * 0.15));
+		tv2.setText("Gluco");
+		tv2.setGravity(Gravity.RIGHT);
+		
+		TextView tv3 = new TextView(this);
+		tv3.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv3.setWidth((int)(metrics.widthPixels * 0.15));
+		tv3.setText("Carbs");
+		tv3.setGravity(Gravity.RIGHT);
+		
+		TextView tv4 = new TextView(this);
+		tv4.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv4.setWidth((int)(metrics.widthPixels * 0.15));
+		tv4.setText("Dose");
+		tv4.setGravity(Gravity.RIGHT);
+		
+		ll.addView(tv1);
+		ll.addView(tv2);
+		ll.addView(tv3);
+		ll.addView(tv4);
+		
+		((LinearLayout) findViewById(R.id.list_entries_header)).addView(ll);
+    }
+    
+    private JournalEntryLinearLayout addRow(ViewGroup target, JournalEntry entry, int accumulatedSpace, Time startTime, String str1, String str2, String str3, String str4, final int id) {
+    	DisplayMetrics metrics = new DisplayMetrics();
+    	getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    	
+    	JournalEntryLinearLayout ll = new JournalEntryLinearLayout(this);
+    	ll.journalEntry = entry;
+		ll.setId(id);
+		ll.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		ll.setOrientation(LinearLayout.HORIZONTAL);
+		if (target.getChildCount() == 0)
+		{
+			ll.setClickable(true);
+			ll.setOnClickListener(new OnClickListener() {
+				
+				public void onClick(View v) {
+					editEntry(id);
+				}
+			});
+			
+		}
+
+		
+		TextView tv1 = new TextView(this);
+		tv1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		tv1.setWidth((int)(metrics.widthPixels * 0.15));
 		tv1.setText(str1);
 		
 		TextView tv2 = new TextView(this);
 		tv2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-		tv2.setWidth((int)(metrics.widthPixels * 0.30));
+		tv2.setWidth((int)(metrics.widthPixels * 0.15));
 		tv2.setText(str2);
+		tv2.setGravity(Gravity.RIGHT);
 		
 		TextView tv3 = new TextView(this);
 		tv3.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-		tv3.setWidth((int)(metrics.widthPixels * 0.25));
+		tv3.setWidth((int)(metrics.widthPixels * 0.15));
 		tv3.setText(str3);
+		tv3.setGravity(Gravity.RIGHT);
 		
 		TextView tv4 = new TextView(this);
 		tv4.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-		tv4.setWidth((int)(metrics.widthPixels * 0.20));
+		tv4.setWidth((int)(metrics.widthPixels * 0.15));
 		tv4.setText(str4);
+		tv4.setGravity(Gravity.RIGHT);
 		
 		ll.addView(tv1);
 		ll.addView(tv2);
@@ -157,6 +256,45 @@ public class GlucoseJournalActivity extends Activity {
 		ll.addView(tv4);
 		
 		target.addView(ll);
+		registerForContextMenu(ll);
+
+		return ll;
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.entry_context_menu, menu);
+        JournalEntry entry = ((JournalEntryLinearLayout)v).journalEntry;
+        menu.setHeaderTitle(entry.at.format("%Y-%m-%d %H:%M"));
+        menu.add(0, v.getId(), 0, "Edit");
+        //menu.add(0, v.getId(), 1, "Change Date");
+        menu.add(0, v.getId(), 2, "Delete");
+    }
+    
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+    	//AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+    	//switch (item.getItemId()) {
+    	//case R.id.edit:
+    	if (item.getTitle() == "Edit") {
+    		editEntry(item.getItemId());
+    		return true;
+    	}
+    	else if (item.getTitle() == "Change Date") {
+    		//changeDateNote(info.id);
+    		return true;
+    	}
+    	else if (item.getTitle() == "Delete") {
+    		dbHandler.deleteJournalEntry(item.getItemId());
+    		refreshEntries();
+    		return true;
+    	}
+    	else {
+    		return super.onContextItemSelected(item);
+    	}
     }
 
 
---
+++ glucosejournal/src/se/huffyreek/glucosejournal/JournalEntryLinearLayout.java
@@ -0,0 +1,13 @@
+package se.huffyreek.glucosejournal;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class JournalEntryLinearLayout extends LinearLayout {
+	public JournalEntryLinearLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JournalEntry journalEntry;
+}
